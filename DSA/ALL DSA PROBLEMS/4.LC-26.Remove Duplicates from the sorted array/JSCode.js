b9a6a67ed53776c1333968dd27bf7081cde2b064
Rishi <rishi.publicbox@gmail.com>
2025-08-29 10:20:35 +0530

    Update JSCode.js

--- DSA/ALL DSA PROBLEMS/4.LC-26.Remove Duplicates from the sorted array/JSCode.js
+++ DSA/ALL DSA PROBLEMS/4.LC-26.Remove Duplicates from the sorted array/JSCode.js
@@ -1,1 +1,53 @@
+### BRUTE FORCE
 
+
+### OPTIMAL
+## CODE - 1 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    if( nums.length == 0 ) return 0;
+    let i = 1
+
+    for( let j = 1 ; j < nums.length ; j++ ){
+        if( nums[j] != nums[ i - 1 ] ){
+            nums[i] = nums[j]
+            i++
+        }
+    }
+    return i 
+};
+
+## CODE - 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    if( nums.length == 0 ) return 0;
+    let i = 0 
+
+    for( let j = 1 ; j < nums.length ; j++ ){
+        if( nums[i] != nums[j] ){
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+};
+
+
+### TIME    → *O*(*n*)
+
+We scan the array once (j goes from 1 to n-1).
+Each comparison/swapping is O(1).
+
+### SPACE → *O*(1)
+
+We only use a few variables (i, j).
+No extra data structures.
